423ed8f634b22dc832d78f29a94694b6e84b8e68
zahra-chaimaa <chaimaamimoun99@gmail.com>
2022-04-14 16:26:28 +0100

    Initial commit

---
+++ Jcolibri-Travel-recommend-main/src/com/demo/app/Test4.java
@@ -0,0 +1,172 @@
+package com.demo.app;
+
+
+
+
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.GlobalSimilarityFunction;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.localSimilarityFunction;
+import es.ucm.fdi.gaia.jcolibri.casebase.LinealCaseBase;
+import es.ucm.fdi.gaia.jcolibri.cbraplications.StandardCBRApplication;
+import es.ucm.fdi.gaia.jcolibri.cbrcore.*;
+import es.ucm.fdi.gaia.jcolibri.exception;
+import es.ucm.fdi.gaia.jcolibri.connector.DataBaseConnector;
+import es.ucm.fdi.gaia.jcolibri.exception.ExecutionException;
+import es.ucm.fdi.gaia.jcolibri.exception.InitializingException;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.NNConfig;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.NNScoringMethod;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.global.Average;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.local.Equal;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.local.Interval;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.RetrievalResult;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.selection.SelectCases;
+import es.ucm.fdi.gaia.jcolibri.util.FileIO;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+
+
+
+public abstract class Test4 implements StandardCBRApplication {
+
+	Connector _connector;
+	CBRCaseBase _caseBase;
+	ArrayList<RetrievalResult>cases;
+	public Collection<CBRCase> casestoreturn;
+	public collection<RetrievalResult>evaltogather;
+
+	public CBRCaseBase mycases;
+	/* (non-Javadoc)
+	 * @see jcolibri.cbraplications.BasicCBRApplication#configure()
+	 */
+	public void configure() throws ExecutionException {
+
+			_connector = new DataBaseConnector();
+			//_connector.initFromXMLfile(jcolibri.util.FileIO.findFile("com.demo.app.databaseconfig.xml"));
+			_connector.initFromXMLfile(FileIO.findFile("com.demo.app.databaseconfig.xml"));
+			_caseBase = (CBRCaseBase) new LinealCaseBase();
+}
+		Collection <CBRCase> mycases;
+
+	
+	/* (non-Javadoc)
+	 * @see jcolibri.cbraplications.BasicCBRApplication#preCycle()
+	 */
+	public CBRCaseBase preCycle() {
+		try {
+			_caseBase.init(_connector);
+		} catch (InitializingException e){
+			e.printStacTrace();
+		}
+		for (CBRCase c : _caseBase.getCases())
+			System.out.Println(c);
+		return _caseBase;
+	}
+	
+	/* (non-Javadoc)
+	 * @see jcolibri.cbraplications.BasicCBRApplication#cycle()
+	 */
+	public void cycle(CBRQuery cbrQuery)
+	{		
+		/********* NumericSim Retrieval **********/
+		
+		NNConfig simConfig = new NNConfig();
+		simConfig.setDescriptionSimFunction(new Average());
+		simConfig.addMapping(new Attribute("Accommodation",com.demo.app.TravelDescription.class),
+				new Equal());
+		Attribute duration = new Attribute("Duration",com.demo.app.TravelDescription.class);
+		simConfig.addMapping(duration, new Interval(31));
+		simConfig.setWeight(duration, 0.5);
+		simConfig.addMapping(new Attribute("HolidayType",com.demo.app.TravelDescription.class), new Equal());
+		simConfig.addMapping(new Attribute("NumberOfPersons",com.demo.app.TravelDescription.class), new Equal());
+		
+		simConfig.addMapping(new Attribute("Region",  com.demo.app.TravelDescription.class), new Average());
+		simConfig.addMapping(new Attribute("region", com.demo.app.Region.class), new Equal());
+		simConfig.addMapping(new Attribute("city",   com.demo.app.Region.class), new Equal());
+		simConfig.addMapping(new Attribute("airport",  com.demo.app.Region.class), new Equal());
+		simConfig.addMapping(new Attribute("currency",com.demo.app.Region.class), new Equal());
+
+		//System.out.println(query);
+		//System.out.println();
+		//Blank description
+		//query.setDescription((CaseComponent) new TravelDescription());
+		/********* Execute NN ************/
+		collection<RetrievalResult> eval = NNScoringMethod.evaluateSimilarity(_caseBase.getCases(), cbrQuery, simConfig);
+		//		Collection<RetrievalResult> eval = NNScoringMethod.evaluateSimilarity(_caseBase.getCases(),
+//				query,
+//				simConfig);
+		
+		/********* Select cases **********/
+		//Collection<CBRCase> selectedcases = SelectCases.selectTopK(eval, 3);
+
+		evaltogather=eval;
+		public void getMeMyCases( int k ) {
+		Collection<CBRCase> selectedcases = SelectCases.selectTopK( evaltogather, k);
+		casestoreturn = selectedcases;
+		//to print the cases after i shortened them to what i want
+		System.out.println("Combined + " + k + " cases gathered");
+		for (CBRCase c : selectedcases)
+			System.out.println(c);
+
+
+
+
+
+
+
+
+
+		
+//		/********* Reuse **********/
+//		// Compute a direct proportion between the "NumberOfPersons" and "Price" attributes.
+//		NumericDirectProportionMethod.directProportion(	new Attribute("NumberOfPersons",TravelDescription.class),
+//													 	new Attribute("price",TravelSolution.class),
+//													 	query, selectedcases);
+//		// Compute a direct proportion between the "Duration" and "Price" attributes.
+//		NumericDirectProportionMethod.directProportion(	new Attribute("Duration",TravelDescription.class),
+//			 											new Attribute("price",TravelSolution.class),
+//			 											query, selectedcases);
+//
+//
+//		Collection<CBRCase> newcases = CombineQueryAndCasesMethod.combine(query, selectedcases);
+//		System.out.println("Combined cases");
+//		for(jcolibri.cbrcore.CBRCase c: newcases)
+//			System.out.println(c);
+//
+//		/********* Revise **********/
+//		CBRCase bestCase = newcases.iterator().next();
+//
+//		HashMap<Attribute, Object> componentsKeys = new HashMap<Attribute,Object>();
+//		componentsKeys.put(new Attribute("caseId",TravelDescription.class), "test3id");
+//		componentsKeys.put(new Attribute("id",TravelSolution.class), "test3id");
+//		//componentsKeys.put(new Attribute("id",Region.class), 7);
+//		jcolibri.method.revise.DefineNewIdsMethod.defineNewIdsMethod(bestCase, componentsKeys);
+//
+//		System.out.println("Case with new Id");
+//		System.out.println(bestCase);
+		
+		/********* Retain **********/
+		
+//		 Uncomment next line to store cases into persistence
+		//jcolibri.method.retain.StoreCasesMethod.storeCase(_caseBase, bestCase);
+	}
+
+	/* (non-Javadoc)
+	 * @see jcolibri.cbraplications.BasicCBRApplication#postCycle()
+	 */
+	public void postCycle() throws ExecutionException {
+		this._caseBase.close();
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {}
+
+
+}
+
+
+//Query definition
---
+++ Jcolibri-Travel-recommend-main/src/com/demo/app/main.java
@@ -0,0 +1,67 @@
+package com.demo.app;
+
+import es.ucm.fdi.gaia.jcolibri.cbrcore.Attribute;
+import es.ucm.fdi.gaia.jcolibri.cbrcore.CBRQuery;
+import es.ucm.fdi.gaia.jcolibri.cbrcore.CaseComponent;
+import es.ucm.fdi.gaia.jcolibri.exception.ExecutionException;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.NNConfig;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.NNScoringMethod;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.global.Average;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.local.Equal;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.local.Interval;
+import es.ucm.fdi.gaia.jcolibri.method.retrieve.RetrievalResult;
+
+import java.util.Collection;
+
+
+
+public class main{
+    public static void main(String[] args) {
+
+        try {
+
+
+            //{
+            /********* Query Definition **********/
+            TravelDescription queryDesc = new TravelDescription();
+            queryDesc.setAccommodation(TravelDescription.AccommodationTypes.ThreeStars);
+            queryDesc.setDuration(10);
+            queryDesc.setHolidayType("Recreation");
+            queryDesc.setNumberOfPersons(4);
+
+            Region region = new Region();
+            region.setRegion("Bulgaria");
+            region.setCity("Sofia");
+            region.setCurrency("Euro");
+            region.setAirport("airport");
+            queryDesc.setRegion(region);
+
+            CBRQuery query = new CBRQuery();
+            query.setDescription((CaseComponent) queryDesc);
+            Test4 test4 =  new Test4() {
+                @Override
+                public void cycle(CBRQuery cbrQuery) throws ExecutionException {
+
+                }
+            };
+            test4.configure();
+            test4.preCycle();
+            test4.cycle(query);
+
+
+            //	System.out.println("Cycle finished. Type exit to idem");
+            //}while(!reader.readLine().equals("exit"));
+
+            test4.postCycle();
+
+        } catch (ExecutionException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }//main
+
+}
